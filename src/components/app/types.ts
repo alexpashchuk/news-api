bcb47cf720df4b623b97475ee67d6c1dfc0cfae6
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-06-10 17:29:08 +0300

    feat: add basic data types

---
+++ src/components/app/types.ts
@@ -0,0 +1,40 @@
+export type InitSource = {
+    id: string;
+    name: string;
+};
+
+export type Articles = {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: InitSource;
+    title: string;
+    url: string;
+    urlToImage: string;
+};
+
+export type Sources = {
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    name: string;
+    url: string;
+    language: string;
+};
+
+export interface NewsData {
+    articles: Array<Articles>;
+    totalResults: number;
+    status: string;
+}
+
+export interface SourceData {
+    sources: Array<Sources>;
+    status: string;
+}
+
+export type AllData = Articles | Sources;
+
+export type Callback<T> = (data?: T) => void;
